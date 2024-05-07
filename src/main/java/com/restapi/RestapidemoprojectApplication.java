fd130bffcc9cc0c9053c2daa8015691c862f5350
Jxmin0412 <123141660+Jxmin0412@users.noreply.github.com>
2024-05-07 14:26:40 +0530

    initial commit

---
+++ src/main/java/com/restapi/RestapidemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapidemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapidemoprojectApplication.class, args);
+	}
+
+}
